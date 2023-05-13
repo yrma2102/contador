1d9ed590173293b35c93eb45ac96d1e63edb2d10
Yrma Ccencho <yrma.2102@gmail.com>
2023-05-12 21:22:59 -0400

    initial project

---
+++ contadorV2/src/main/java/com/example/contadorV2/ContadorV2Application.java
@@ -0,0 +1,13 @@
+package com.example.contadorV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorV2Application.class, args);
+	}
+
+}
---
+++ contadorV2/src/test/java/com/example/contadorV2/ContadorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contadorV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
